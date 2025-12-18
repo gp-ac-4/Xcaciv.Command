e077c3545c8616791171676995879e8063e44b22
xcaciv <23732819+Xcaciv@users.noreply.github.com>
2025-12-18 08:51:21 -0700

    added command result record

---
+++ src/Xcaciv.Command.Interface/CommandResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xcaciv.Command.Interface
+{
+    public record CommandResult
+    {
+        public bool IsSuccess { get; init; }
+        public string? ErrorMessage { get; init; }
+        public Exception? Exception { get; init; }
+        public string CorrelationId { get; init; } = Guid.NewGuid().ToString();
+        public string? Output { get; init; }
+    }
+}
